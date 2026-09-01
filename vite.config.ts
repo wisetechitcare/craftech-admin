f4600f36e6bdda3b7bd6d7265c0ffdbcb2de4862
Mustafa Khan <sahil.k31499@gmail.com>
2026-09-01 10:53:57 +0530

    Removed Unecessary .env & changes in vite.config.ts for Dynamic values

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })
